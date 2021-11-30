b549b4d874834e4129c71c076d22d81e67dd7702
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-11-30 14:45:23 +0530

    Added validator

---
+++ middleware/validate.middleware.js
@@ -0,0 +1,35 @@
+const joi = require('joi');
+const validateMiddleware = require('koa-joi-validate-middleware');
+
+const errorCallback = (ctx, error) => {
+  ctx.status = 422;
+  ctx.body = error.message;
+};
+
+const postItemsSchema = {
+  body: joi.object({
+    name: joi.string().min(1).max(40).required(),
+    price: joi.number().positive().precision(2),
+  }),
+};
+
+const postUsersSchema = {
+  body: joi.object({
+    name: joi.string().min(1).max(40).required(),
+    address: joi.string().required(),
+  }),
+};
+
+const postCartSchema = {
+  params: joi.object({
+    userId: joi.number().integer().positive().required(),
+  }),
+  body: joi.object({
+    itemId: joi.number().positive().integer().required(),
+    quantity: joi.number().positive().integer().required(),
+  }),
+};
+
+exports.postItems = validateMiddleware.create(postItemsSchema, errorCallback);
+exports.postUsers = validateMiddleware.create(postUsersSchema, errorCallback);
+exports.postCart = validateMiddleware.create(postCartSchema, errorCallback);
--- routes.js
+++ routes.js
@@ -1,23 +1,24 @@
 const Router = require('koa-router');
 const router = new Router();
 
+const validators = require('./middleware/validate.middleware');
 const Items = require('./src/apis/item');
 const Users = require('./src/apis/user');
 const Carts = require('./src/apis/cart');
 
-router.post('/items', Items.add);
+router.post('/items', validators.postItems, Items.add);
 router.get('/items/:id', Items.get);
 router.get('/items', Items.getAll);
 router.put('/items/:id', Items.update);
 router.delete('/items/:id', Items.delete);
 
-router.post('/users', Users.add);
+router.post('/users', validators.postUsers, Users.add);
 router.get('/users/:id', Users.get);
 router.get('/users', Users.getAll);
 router.put('/users/:id', Users.update);
 router.delete('/users/:id', Users.delete);
 
-router.post('/carts/:userId', Carts.add);
+router.post('/carts/:userId', validators.postCart, Carts.add);
 // curl -i "localhost:3000/carts/3?desc=true&offset=2&limit=2"
 router.get('/carts/:userId', Carts.get);
 
